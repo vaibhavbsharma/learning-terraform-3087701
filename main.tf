e02c6b8142de4ab39ddfa057f73bae58c046e682
Vaibhav Sharma <vaibhavbhushansharma@gmail.com>
2026-07-19 08:41:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs*"]
   }
 
   filter {
